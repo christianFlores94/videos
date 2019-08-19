1d11faf7e65b81ac00f00d39f31d29354e22f06d
christianFlores94 <christianalexisflores@hotmail.com>
2019-08-18 21:20:51 -0300

    nuevo

--- shellcontrol.py
+++ shellcontrol.py
@@ -5,7 +5,9 @@ import subprocess
 from subprocess import run, PIPE
 import multiprocessing
 import time
-
-os.system("./add.py")
-os.system("./commit.py")
-os.system("./user.py | git push")
+os.system("gnome-terminal -e 'bash -c \"./add.py; exec bash\"'")
+#os.system("./add.py")
+os.system("gnome-terminal -e 'bash -c \"./commit.py; exec bash\"'")
+os.system("gnome-terminal -e 'bash -c \"./user.py | git push; exec bash\"'")
+#os.system("./commit.py")
+#os.system("./user.py | git push")
