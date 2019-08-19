fcc7430e68a198b7ee0da90061d1ab9ddfa2db83
christianFlores94 <christianalexisflores@hotmail.com>
2019-08-18 21:21:35 -0300

    nuevo

--- shellcontrol.py
+++ shellcontrol.py
@@ -8,6 +8,6 @@ import time
 os.system("gnome-terminal -e 'bash -c \"./add.py; exec bash\"'")
 #os.system("./add.py")
 os.system("gnome-terminal -e 'bash -c \"./commit.py; exec bash\"'")
-os.system("gnome-terminal -e 'bash -c \"./user.py | git push; exec bash\"'")
+#os.system("gnome-terminal -e 'bash -c \"./user.py | git push; exec bash\"'")
 #os.system("./commit.py")
 #os.system("./user.py | git push")
